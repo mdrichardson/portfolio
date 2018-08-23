79aaf2357fdf22936aedb3bb73075c73efd60a07
Michael Richardson <michaelgocougs@gmail.com>
2018-08-22 18:23:48 -0700

    moved gloval sass into index instead of app.scss
    fixed body background gradient so it doesn't grow

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './projects.css';
+import ProjectsList from './ProjectsList';
+import projectsTitle from '../images/projects.svg'
 
 const Projects = () => {
     return (
         <div id="Projects">
+            <ProjectsList />
+            <div className="section-title">
+                <img src={ projectsTitle } alt="Projects" />
+            </div>
         </div>
     );
 }
---
+++ src/projects/ProjectsList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ProjectsList = (props) => (
+    <div id='1'>
+        
+    </div>
+)
+        
+
+export default ProjectsList;
