33990c9eeffa4fd858dcee6b389a0505d2fffb6e
Michael Richardson <michaelgocougs@gmail.com>
2018-08-28 16:53:23 -0700

    fancy welcome message

--- src/App.js
+++ src/App.js
@@ -8,6 +8,30 @@ import About from './about/About';
 import Contact from './contact/Contact';
 import Waypoint from 'react-waypoint';
 
+// Styling for fancy messages in dev console
+const logFName = [
+    'background: #F71735',
+    'color: white',
+    'font-size: 20px'
+].join(';');
+
+const logLName = [
+    'background: #FCAB10',
+    'color: white',
+    'font-size: 20px'
+].join(';');
+
+const logHeader = [
+    'color: black',
+    'font-size: 16px',
+    'font-weight: bold'
+].join(';');
+
+const logMessage = [
+    'color: black',
+    'font-size: 14px'
+].join(';');
+
 class App extends Component {
 
     constructor(props) {
@@ -19,6 +43,15 @@ class App extends Component {
 
     fixNav = () => this.setState({ navFixed: true });
     unfixNav = () => this.setState({ navFixed: false });
+
+    componentDidMount() {
+        // Fancy welcome message in dev console
+        console.log('%c  Michael %c Richardson  ', logFName, logLName);
+        console.log('%cWelcome to my portfolio!', logHeader);
+        console.log('%cIf you\'re reading this, you\'re either trying to break my page or see ' +
+                    'if I\'m a half decent developer. If you think I am, contact me:', logMessage);
+        console.log('%chttps://www.mdrichardson.net/#Contact', logMessage);
+    }
         
     render() {
         return (
