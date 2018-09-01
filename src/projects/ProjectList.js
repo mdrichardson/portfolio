90899cac05ecd205000623581badf42d04971843
Michael Richardson <michaelgocougs@gmail.com>
2018-08-31 18:24:50 -0700

    more responsive to 768px

--- src/projects/ProjectList.js
+++ src/projects/ProjectList.js
@@ -27,41 +27,40 @@ class ProjectsList extends React.Component {
                 <div key={proj.id} id={proj.id}
                     className={`project-container ${this.state.expandedKey === proj.id ? 'expanded': ''}`}
                     onMouseEnter={() => this.expandIt(proj.id)} onMouseLeave={() => this.collapseIt(proj.id)}>
-                    <div className="text">
+                    <div className="project-text">
                         <h2>{proj.name }</h2>
                         <div className="short-desc">
                             <p>{proj.short_desc}</p>
                         </div>
-                        <ul className="bullets">
-                            <ProjectBullets list={proj.bullets}/>
-                        </ul>
-                        <div className="long-desc">
-                            <p>{proj.long_desc}</p>
-                        </div>
                     </div>
+                    <div className="long-desc">
+                        <p>{proj.long_desc}</p>
+                    </div>
+                    <ul className="bullets">
+                        <ProjectBullets list={proj.bullets}/>
+                    </ul>
                     <div className="main-image" onClick={() => this.toggleExpand(proj.id)}>
                         <img src={proj.image} alt={proj.name}></img>
-                        {/* We need to duplicate the tool icons so creating the expanded layout is easier */}
                         <div className="expanded-tool-icons">
                             <ToolIcons list={proj.tools} />
-                        </div>
-                        <div className="links">
-                            <div className="github-link">
-                                <a href={proj.github} alt="Github link" target="_blank">
-                                    <FontAwesomeIcon icon={faGithub} />
-                                    <span>Github Repo</span>
-                                </a>
-                            </div>
-                            <div className="web-link" hidden={proj.url ? false : true}>
-                                <a href={proj.url} alt="Project link" target="_blank">
-                                    <FontAwesomeIcon icon={faGlobe} />
-                                    <span>Website</span>
-                                </a>
-                            </div>
                         </div>
                     </div>
                     <div className="tool-icons">
                         <ToolIcons list={proj.tools} />
+                    </div>
+                    <div className="links">
+                        <div className="github-link">
+                            <a href={proj.github} alt="Github link" target="_blank">
+                                <FontAwesomeIcon icon={faGithub} />
+                                <span>Github Repo</span>
+                            </a>
+                        </div>
+                        <div className="web-link" hidden={proj.url ? false : true}>
+                            <a href={proj.url} alt="Project link" target="_blank">
+                                <FontAwesomeIcon icon={faGlobe} />
+                                <span>Website</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             ))
