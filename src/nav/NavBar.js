572d2ca588490518fa96d07e97102dc6cef1181c
Michael Richardson <michaelgocougs@gmail.com>
2018-09-01 17:57:36 -0700

    nav great at 800px and animated

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -10,40 +10,88 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 // props.active is set in App.js, using Waypoints. This sets a white underline to the active component
 
-const NavBar = (props) => {
-    return(
-        <div>
-            <AppBar position="static" id="nav" className={ props.navFixed ? "fixed" : "" }>
-                <Toolbar id="toolbar">
-                    <Typography variant="title" id="name">
-                    <span className={ !props.navFixed ? "hidden" : "" }>Michael Richardson</span>
-                    </Typography>
-                    <div id="menu-container">
-                        <ul id="menu">
-                            <li className="navItem"><div>
-                                <NavLink to="/#Home" smooth>Home</NavLink>
-                            </div></li>
-                            <li className={ props.active === 'skills' ? "active navItem" : "navItem" }><div>
-                                <NavLink to="/#Skills" smooth>Skills</NavLink>
-                            </div></li>
-                            <li className={ props.active === 'projects' ? "active navItem" : "navItem" }><div>
-                                <NavLink to="/#Projects" smooth>Projects</NavLink>
-                            </div></li>
-                            <li className={ props.active === 'about' ? "active navItem" : "navItem" }>
-                                <div><NavLink to="/#About" smooth>About</NavLink>
-                            </div></li>
-                            <li className={ props.active === 'contact' ? "active navItem" : "navItem" }><div>
-                                <NavLink to="/#Contact" smooth>Contact</NavLink>
-                            </div></li>
-                        </ul>
-                    </div>
-                    <div id="hamburger-menu">
-                        <FontAwesomeIcon icon={faBars} size="2x" />
-                    </div>
-                </Toolbar>
-            </AppBar>
-            <hr id ="menu-hr" />
-        </div>
-    )
+class NavBar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            animateClassName: 'load'
+        }
+    }
+
+    toggleMenuAnimation = () => {
+        // We need a separate state for animateClassName so that animation doesn't start on page load--only when menu is expanded
+        let animateClassName = this.state.animateClassName === 'animated' ? '' : 'animated' ;
+        this.setState({ animateClassName })
+        // If menu is open, we need to make sure it closes
+        if (this.state.animateClassName === 'animated') {
+            this.closeMenu();
+        // We need to set the bottom margin of the hr dynamically so it only applies when it's visible, which is when we're at the top of the page
+        } else if (window.scrollY < 64){
+            document.getElementById('menu-hr').style.marginBottom = '100vh';
+        } 
+        if (this.state.animateClassName !== 'animated') {
+            this.watchScroll();
+        }
+    }
+
+    // Watch for scrolling when menu is open and close menu when scroll initiated
+    watchScroll = () => document.addEventListener('scroll', this.closeMenu)
+
+    // Always close the menu when we click on the inner links
+    closeMenu = () => {
+        this.setState({ animateClassName: '' });
+        document.getElementById('menu-hr').style.marginBottom = '0';
+        document.removeEventListener('scroll', this.closeMenu)
+    }
+
+    // When we add the bottom margin to the <hr> it moves everything down 100vh, so we need to scroll that much further when links are clicked
+    customScroll = (el) => {
+        const offset = window.innerWidth <= 800 && window.scrollY < 64 ? window.innerHeight : 0;
+        const position = el.offsetTop - offset;
+        window.scroll({
+            top: position,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <AppBar position="static" id="nav" className={ this.props.navFixed ? "fixed" : "" }>
+                    <Toolbar id="toolbar">
+                        <Typography variant="title" id="name">
+                        <span className={ !this.props.navFixed ? "hidden" : "" }>Michael Richardson</span>
+                        </Typography>
+                        <div id="menu-container" className={ this.state.animateClassName }>
+                            <ul id="menu">
+                                <div id="menu-background"  className={ this.state.animateClassName } style={ window.scrollY >= 64 ? { backgroundColor: '#F71735' } : {} }></div>
+                                <li className="navItem"><div>
+                                    <NavLink to="/#Home" scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })} onClick={ this.closeMenu }>Home</NavLink>
+                                </div></li>
+                                <li className={ this.props.active === 'skills' ? "active navItem" : "navItem" }><div>
+                                    <NavLink to="/#Skills" scroll={el => this.customScroll(el) } onClick={ this.closeMenu }>Skills</NavLink>
+                                </div></li>
+                                <li className={ this.props.active === 'projects' ? "active navItem" : "navItem" }><div>
+                                    <NavLink to="/#Projects" scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })} onClick={ this.closeMenu }>Projects</NavLink>
+                                </div></li>
+                                <li className={ this.props.active === 'about' ? "active navItem" : "navItem" }>
+                                    <div><NavLink to="/#About" scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })} onClick={ this.closeMenu }>About</NavLink>
+                                </div></li>
+                                <li className={ this.props.active === 'contact' ? "active navItem" : "navItem" }><div>
+                                    <NavLink to="/#Contact" scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })} onClick={ this.closeMenu }>Contact</NavLink>
+                                </div></li>
+                            </ul>
+                        </div>
+                        <div id="hamburger-menu" onClick={ this.toggleMenuAnimation }>
+                            <FontAwesomeIcon icon={faBars} size="2x" />
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                <hr id ="menu-hr" className={ this.state.animateClassName }/>
+            </div>
+        )
+    }
 }
 export default NavBar;
