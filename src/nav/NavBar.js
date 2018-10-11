8d43d53d2f0c30177928fad4c42fe3af41c36be3
Michael Richardson <michaelgocougs@gmail.com>
2018-10-11 11:32:49 -0700

    fixed nav links after moving into separate file

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -19,6 +19,19 @@ class NavBar extends React.Component {
         }
     }
 
+    // Always close the menu when we click on the inner links
+    closeMenu = () => {
+        // Don't close the menu if it isn't shown anyway -- keeps hr from animating
+        if (window.innerWidth <= 800) {
+            this.setState({ animateClassName: '' });
+            document.getElementById('menu-hr').style.marginBottom = '0';
+            document.removeEventListener('scroll', this.closeMenu)
+        }
+    }
+
+    // Watch for scrolling when menu is open and close menu when scroll initiated
+    watchScroll = () => document.addEventListener('scroll', this.closeMenu)
+
     toggleMenuAnimation = () => {
         // Only animate if hamburger menu shown (smaller screens)
         if (window.innerWidth <= 800) {
@@ -37,30 +50,6 @@ class NavBar extends React.Component {
                 this.watchScroll();
             }
         }
-    }
-
-    // Watch for scrolling when menu is open and close menu when scroll initiated
-    watchScroll = () => document.addEventListener('scroll', this.closeMenu)
-
-    // Always close the menu when we click on the inner links
-    closeMenu = () => {
-        // Don't close the menu if it isn't shown anyway -- keeps hr from animating
-        if (window.innerWidth <= 800) {
-            this.setState({ animateClassName: '' });
-            document.getElementById('menu-hr').style.marginBottom = '0';
-            document.removeEventListener('scroll', this.closeMenu)
-        }
-    }
-
-    // When we add the bottom margin to the <hr> it moves everything down 100vh, so we need to scroll that much further when links are clicked
-    customScroll = (el) => {
-        const offset = window.innerWidth <= 800 && window.scrollY < 64 ? window.innerHeight : 0;
-        const position = el.offsetTop - offset;
-        window.scroll({
-            top: position,
-            left: 0,
-            behavior: 'smooth'
-        })
     }
 
     render() {
--- src/nav/NavLinks.js
+++ src/nav/NavLinks.js
@@ -3,6 +3,34 @@ import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 class NavLinks extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            animateClassName: 'load'
+        }
+    }
+
+    // Always close the menu when we click on the inner links
+    closeMenu = () => {
+        // Don't close the menu if it isn't shown anyway -- keeps hr from animating
+        if (window.innerWidth <= 800) {
+            this.setState({ animateClassName: '' });
+            document.getElementById('menu-hr').style.marginBottom = '0';
+            document.removeEventListener('scroll', this.closeMenu)
+        }
+    }
+
+    // When we add the bottom margin to the <hr> it moves everything down 100vh, so we need to scroll that much further when links are clicked
+    customScroll = (el) => {
+        const offset = window.innerWidth <= 800 && window.scrollY < 64 ? window.innerHeight : 0;
+        const position = el.offsetTop - offset;
+        window.scroll({
+            top: position,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+
     render() {
         return(
             <ul id="menu">
@@ -17,6 +45,9 @@ class NavLinks extends React.Component {
                 </div></li>
                 <li className={ this.props.active === 'about' ? "active navItem" : "navItem" }>
                     <div><NavLink to="/#About" scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })} onClick={ this.closeMenu }>About</NavLink>
+                </div></li>
+                <li className={ this.props.active === 'blog' ? "active navItem" : "navItem" }>
+                    <div><NavLink to="/Blog" onClick={ this.closeMenu }>Blog</NavLink>
                 </div></li>
                 <li className={ this.props.active === 'contact' ? "active navItem" : "navItem" }><div>
                     <NavLink to="/#Contact" scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'start' })} onClick={ this.closeMenu }>Contact</NavLink>
